3b71396cf2199da2c98e1d94d94925dd77cc8500
unknown <carlos.rodriguez12@uptc.edu.co>
2019-07-13 16:25:36 -0500

    :v

---
+++ pedidos/config/db.php
@@ -0,0 +1,11 @@
+﻿<?php
+	/*-------------------------
+	Autor: Obed Alvarado
+	Web: obedalvarado.pw
+	Mail: info@obedalvarado.pw
+	---------------------------*/
+define("DB_HOST", "35.239.170.13");//DB_HOST:  generalmente suele ser "127.0.0.1"
+define("DB_NAME", "pedidos");//Nombre de la base de datos
+define("DB_USER", "root");//Usuario de tu base de datos
+define("DB_PASS", "root");//Contraseña del usuario de la base de datos
+
